93d890c8b35f9bcc2d1a5ec1c5e08c355780306e
mikkikiki656 <abobamikki666@gmail.com>
2026-06-27 00:47:37 +0300

    Create задача 16 .cpp

---
+++ задача 16 .cpp
@@ -0,0 +1,5 @@
+void sortStrings(string* arr, int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                string
